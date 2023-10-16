d77d5296f9524fe97b622d2291ba8ddee41d499f
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-16 11:54:30 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = License.CC0_1_0()
+LICENSE: License = License.CC0_1_0(source_url="https://github.com/m0-n/Plastic-Bottles-Dataset#license")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.Environmental(is_used=False)]
 CATEGORY: Category = Category.Environmental()
 
